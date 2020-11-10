064372ae09526fd59982b636ebfe1112dc806e52
Jared Suda <jtssuda@hotmail.com>
2020-11-10 06:27:10 -0700

    Added product stack functionality for cart

--- static/javascript/cart.js
+++ static/javascript/cart.js
@@ -5,8 +5,31 @@
     for(let i=0; i<prices.length; i++){
         totalPrice += parseFloat(prices[i].innerHTML.slice(1));
     }
-    document.getElementById("total-price").innerHTML = totalPrice;
+    document.getElementById("total-price").innerHTML = totalPrice.toFixed(2);
+
+
+
+
+    // Combining Multiple Products Into Same Stack
+    const productNames = document.querySelectorAll(".product-name");
+    let count = 0;
+    for(let i=count; i<productNames.length; i++){
+        let duplicateTest = productNames[i].innerHTML;
+        count = 0;
+        for(let x=0; x<productNames.length; x++){
+            if(duplicateTest === productNames[x].innerHTML){
+                count += 1;
+                if(count>1){
+                    productNames[x].parentElement.parentElement.parentElement.style.display = "none";
+                }
+            }
+        }
+        // Displaying Product Quantity
+        document.querySelectorAll(".product-qty")[i].value = count;
+        
+    }
 
 
 
 }
+
